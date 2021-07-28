8c5e554fcc48d592c2adcda294f1cead478ae747
andersonotavio <andersonotavio@hotmail.com>
2021-07-28 11:33:17 -0300

    first commit api gitHub

---
+++ src/components/no-search/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 16px;
+  display: flex;
+  justify-content: center;
+  margin: 150px auto;
+
+  h1 {
+    font-weight: bold;
+    font-size: 32px;
+  }
+`;
---
+++ src/components/profile/styled.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  @media (max-width: 710px){
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const WrapperInfoUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin-left: 8px;
+  height: 200px;
+
+  h1 {
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: bold;
+  }
+`;
+
+export const WrapperStatusCount = styled.div`
+  display: flex;
+  align-items: center;
+  div {
+    margin: 8px;
+    text-align: center;
+  }
+`;
+
+export const WrapperUserGeneric = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+
+  h3 {
+    margin-right: 8px;
+  }
+
+  a {
+    font-size: 18px;
+    color: blue;
+    font-weight: bold;
+  }
+`;
+
+export const WrapperImage = styled.img`
+  border-radius: 50%;
+  width: 200px;
+  margin: 8px;
+`;
